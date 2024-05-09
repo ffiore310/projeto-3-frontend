86de420daeb7cc4793545a1ede4473b5fadd6351
Fernando Fiore <fernandoof@al.insper.edu.br>
2024-05-09 11:18:56 -0300

    comecando os favoritos

---
+++ infosport-frontend/components/favoritos/index.jsx
@@ -0,0 +1,13 @@
+export default function Favoritos() {
+    return (
+        <div className="favoritos">
+            <h1>Times Favoritos</h1>
+            <div className="timesFavoritos">
+                <div className="time">
+                    <h2>Time 1</h2>
+                    <button>Remover dos Favoritos</button>
+                </div>
+            </div>
+        </div>
+    );
+}
--- infosport-frontend/components/header/index.jsx
+++ infosport-frontend/components/header/index.jsx
@@ -1,10 +1,12 @@
 import './style.css';
+import { Link } from "react-router-dom";
 
 export default function Header() {
     return (
         <div className="header">
             <img src="/logo.png" alt="Logo" className="logo" />
-            <h1 className="appName">InsperBet</h1>
+            <h1 className="appName">InsperBrasileirao</h1>
+            <Link to={`favoritos`}>✏️</Link>
         </div>
     );
 }
