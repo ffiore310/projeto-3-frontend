3d140ce1d91f2dfce50b45b7c37848f0b3de2122
Fernando Fiore <fernandoof@al.insper.edu.br>
2024-05-09 08:53:54 -0300

    deixando a estrutura do axios pronta

--- infosport-frontend/src/App.jsx
+++ infosport-frontend/src/App.jsx
@@ -1,9 +1,22 @@
-
+import axios from "axios";
 import './App.css'
 import Header from '../components/header'
 import Cadastro from '../components/cadastro'
 
 function App() {
+
+  const config = {headers: {
+    'X-RapidAPI-Key': 'e1c9dc4910msha22f12bf8ae6ee5p121aedjsnc98f63a23ecd',
+    'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+  }};
+  axios
+    .get(`https://wft-geo-db.p.rapidapi.com/v1/geo/countries/BR/regions/${isoCode}/`, config)
+    .then((res) => {
+      setDetalhes(res.data.data);
+      console.log(res.data.data);
+    });
+
+
 
   return (
     <div>
