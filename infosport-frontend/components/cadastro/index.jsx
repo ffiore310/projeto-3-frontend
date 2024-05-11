00a262f3f4c9ac4b4725f02ace083ffd0370559f
arthurcalabresi <103602813+arthurcalabresi@users.noreply.github.com>
2024-05-11 17:40:55 -0300

    salvamento de ususario funcionando

--- infosport-frontend/components/cadastro/index.jsx
+++ infosport-frontend/components/cadastro/index.jsx
@@ -8,43 +8,45 @@ export default function Cadastro() {
     const [email, setEmail] = useState("");
     const [senha, setSenha] = useState("");
 
-    const criaUsuario = (event) => {
-        event.prentDefault();
+    async function sendUser(event) {
+        event.preventDefault();
 
         const data = {
-            "nome": username,
-            "emial": email,
-            "senha": senha
+            "username": username,
+            "email": email,
+            "password": senha
         }
+
         const options = {
             method: 'POST',
-            url: 'http://localhost:8000/usuario',
+            url: 'http://localhost:8000/api/users/',
             headers: {accept: 'application/json'},
             data: data
         }
 
-        try{
-            axios.request(options)
-            console.log("Sucesso ao criar ususario")
+        try {
+            await axios.request(options);
+            console.log("Sucesso ao criar usuário");
         } catch (error) {
-            console.error("Falha ao criar ususario", error)
+            console.error("Falha ao criar usuário", error);
         }
     }
 
     return(
-        <form onSubmit={criaUsuario}>
+        <form onSubmit={sendUser}>
             <div>
                 <label>Username:</label>
-                <input type="text" name="username" placeholder="Digite seu username" onChange={(event) => {setUsername(event.target.value), console.log(event.target.value)}} />
-            </div>
-            <div>
-                <label>Email:</label>
-                <input type="email" name="email" placeholder="Digite seu email"  onChange={(event) => setEmail(event.target.value)} />
+                <input type="text" name="username" placeholder="Digite seu username" onChange={(event) => setUsername(event.target.value)} />
             </div>
             <div>
                 <label>Password:</label>
                 <input type="senha" name="senha" placeholder="Digite sua senha" onChange={(event) => setSenha(event.target.value)} />
             </div>
+            <div>
+                <label>Email:</label>
+                <input type="email" name="email" placeholder="Digite seu email"  onChange={(event) => setEmail(event.target.value)} />
+            </div>
+            
             <button type="submit">Sign Up</button>
         </form>
     );
