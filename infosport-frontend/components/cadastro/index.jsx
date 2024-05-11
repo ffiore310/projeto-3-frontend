225307b10aa0a64799a17a57851cefb380a31e0b
arthurcalabresi <103602813+arthurcalabresi@users.noreply.github.com>
2024-05-11 18:13:19 -0300

    front de cadastro funcionando (nao decorado)

--- infosport-frontend/components/cadastro/index.jsx
+++ infosport-frontend/components/cadastro/index.jsx
@@ -1,14 +1,19 @@
 import axios from "axios";
 import { useState } from "react";
+import { useNavigate } from 'react-router-dom';
 
 
 export default function Cadastro() {
+    const navigate = useNavigate();
 
     const [username, setUsername] = useState('');
     const [email, setEmail] = useState("");
     const [senha, setSenha] = useState("");
+    const [msgSucesso, setMsgSucesso] = useState(false);
+    const [error, setError] = useState('');
 
     async function sendUser(event) {
+        
         event.preventDefault();
 
         const data = {
@@ -27,8 +32,16 @@ export default function Cadastro() {
         try {
             await axios.request(options);
             console.log("Sucesso ao criar usuário");
+            setMsgSucesso(true);
+            setTimeout(() => navigate('/'), 2000);
+  
         } catch (error) {
             console.error("Falha ao criar usuário", error);
+            if (error.response && error.response.status === 500) {
+                setError('Este nome de usuário já está em uso. Insira outro');
+            } else {
+                setError('Ocorreu um erro ao criar o usuário. Tente novamente.');
+            }
         }
     }
 
@@ -48,6 +61,12 @@ export default function Cadastro() {
             </div>
             
             <button type="submit">Sign Up</button>
+            <div className="msgSucesso">
+                {msgSucesso && <p>Usuário criado com sucesso! Redirecionando para a Home...</p>}
+            </div>
+            <div className="msgError">
+                {error && <p>{error}</p>}
+            </div>
         </form>
     );
 }
