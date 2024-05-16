010738f8e49cac31a48d60fa11ce4b18674d01df
eduardomontandon <emontandon3@gmail.com>
2024-05-16 09:17:01 -0300

    terminado minha parte

--- infosport-frontend/components/times/index.jsx
+++ infosport-frontend/components/times/index.jsx
@@ -1,55 +1,36 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 
-
-
 function Times() {
     const [teams, setTeams] = useState([]);
-    //const options = {headers: {accept: 'application/json'}};
-  
-    useEffect(() => {
-        
 
+    useEffect(() => {
         const options = {
             method: 'GET',
             url: "http://localhost:8000/api/soccer/",
-            //params: {api_key: 'o4FFngW73VpSgSoIadrB8seMLGUwvok1n5HNY8Zc'},
-            //headers: {accept: 'application/json'}
-            };
+        };
 
-            axios
+        axios
             .request(options)
             .then(function (response) {
-                console.log(response.data);
+                console.log(response.data.season_competitors);
+                setTeams(response.data.season_competitors);
             })
             .catch(function (error) {
                 console.error(error);
             });
-    //   axios
-    //     .get('https://api.sportradar.com/soccer/trial/v4/en/seasons/sr%3Aseason%3A113943/competitors.json?api_key=o4FFngW73VpSgSoIadrB8seMLGUwvok1n5HNY8Zc', options)
-    //     .then((res) => {
-    //         // Extrair os nomes dos times da resposta
-    //         console.log("meu nome é eduardo")
-    //         console.log(res)
-    //         //const teamNames = res.data.competitors.map(team => team.name);
-    //         //setTeams(teamNames);
-    //       })
-    //       .catch((error) => {
-    //         console.error('Erro ao buscar os times:', error);
-    //       });
-      }, []);
-  
-  
-      return (
+    }, []);
+
+    return (
         <div>
-          <h1>Nomes dos Times:</h1>
-          <ul>
-            {teams.map((team, index) => (
-              <li key={index}>{team}</li>
-            ))}
-          </ul>
+            <h1>Nomes dos Times:</h1>
+            <ul>
+                {teams.map((team) => (
+                    <li key={team.id}>{team.name}</li>
+                ))}
+            </ul>
         </div>
-      );
-    }
-    
-    export default Times;
+    );
+}
+
+export default Times;
