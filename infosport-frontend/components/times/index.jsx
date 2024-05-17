b6099f42ab97dc423d20d80baf239bcd54d561b1
Fernando Fiore <fernandoof@al.insper.edu.br>
2024-05-17 17:24:14 -0300

    adicionando detalhes

--- infosport-frontend/components/times/index.jsx
+++ infosport-frontend/components/times/index.jsx
@@ -5,6 +5,8 @@ import { useEffect, useState } from "react";
 
 function Times() {
     const [teams, setTeams] = useState([]);
+    const [message, setMessage] = useState('');
+    const [favoritedTeams, setFavoritedTeams] = useState([]);
     let token = localStorage.getItem('token');
 
     useEffect(() => {
@@ -27,6 +29,12 @@ function Times() {
     }, []);
 
     function favoritarTime(team) {
+      if (favoritedTeams.includes(team)) {
+        setMessage(`O time ${team} já está favoritado!`);
+        setTimeout(() => setMessage(''), 1500); // Limpa a mensagem após 5 segundos
+        return;
+    }
+
       console.log("Favoritando time", team);
       const url = "http://localhost:8000/api/favorita/"; // URL do endpoint do backend Django
       const body = {'name': team}; // ou qualquer dado específico que o backend precisa
@@ -38,6 +46,9 @@ function Times() {
       axios.post(url, body, config)
         .then((res) => {
           console.log(res.data);
+          setFavoritedTeams(prev => [...prev, team]); // Adiciona o time à lista de favoritados
+          setMessage(`Time ${team} favoritado com sucesso!`);
+          setTimeout(() => setMessage(''), 1500);
         });
     }
   
@@ -45,6 +56,7 @@ function Times() {
       return (
         <div>
           <h1>Nomes dos Times:</h1>
+          {message && <p>{message}</p>} {/* Mostra a mensagem de feedback */}
           <ul>
             {teams.map((team, index) => (
               <li key={index}>
